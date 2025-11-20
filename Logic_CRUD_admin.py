589421846e41a35a263f4c54ce3104c07fdde074
Alfauzi-S <muhammad.alfauzi256@gmail.com>
2025-11-21 00:43:42 +0800

    simpan

--- Logic_CRUD_admin.py
+++ Logic_CRUD_admin.py
@@ -7,36 +7,33 @@ import Essential_hendling_error as ehr
 from colorama import init, Fore, Style
 init(autoreset=True)
 
+# status done
 def create():
     tool.clear()
-    print(f"{'='*18}{'TAMBAH PRODUK BARU'}{'='*18}")
+    print(Fore.YELLOW + f"{" TAMBAH PRODUK BARU ":=^50}")
     id_baru = ehr.harus_nomor("Masukkan ID Produk baru   : ")
-    
-    #cek id
-    if id_baru in dpm.data_produk:
-        print(f"\nID {id_baru} sudah ada.")
-    else:
-        #tersedia
-        print(f"ID {id_baru} tersedia. Silakan masukkan detail produk:")
+    if id_baru not in dpm.data_produk:
         produk = ehr.tidak_kosong_capitalize("Nama Produk               : ")
         stok = ehr.harus_nomor("Stok Awal                 : ")
         ukuran = ehr.harus_nomor("Ukuran/per stok (Kg)      : ")
         harga = ehr.harus_nomor("Harga                     : ")
         tgl_masuk = ehr.input_tanggal("Tanggal Masuk (YYYY-MM-DD): ")
+
         while True:
             kadaluarsa = ehr.input_tanggal("Kadaluarsa (YYYY-MM-DD)   : ")
-            if kadaluarsa > tgl_masuk :
+            if kadaluarsa > tgl_masuk:
                 break
             else:
-                print("Tanggal kadaluarsa harus lebih besar dari tanggal masuk.")
+                print(Fore.RED + "Tanggal kadaluarsa harus lebih besar dari tanggal masuk.")
+        
+        print("\nkatagori: 1.Ruminansia 2.Unggas 3.Perikanan 4.Hewan Peliharaan" )
         while True:
-            print("\nkatagori: 1.Ruminansia 2.Unggas 3.Perikanan 4.Hewan Peliharaan" )
             pilihan = ehr.input_menu("pilih kategori (1/2/3/4)  :")
             if pilihan in [1, 2, 3, 4]:
                 kategori = 'Ruminansia' if pilihan == 1 else 'Unggas' if pilihan == 2 else 'Perikanan' if pilihan == 3 else 'Hewan Peliharaan'
                 break
             else: 
-                print('Pilihan tidak valid!.')
+                print(Fore.RED + 'Pilihan tidak valid!.')
 
         data_baru = {
             "Nama Produk": produk,
@@ -48,82 +45,92 @@ def create():
             "Kategori": kategori
         }
         dpm.data_produk[id_baru] = data_baru
-            
-        print(f"\nData Pakan '{produk}' (ID: {id_baru}) berhasil ditambahkan!")
-    
+        print(Fore.CYAN + f"\nData Pakan '{produk}' (ID: {id_baru}) berhasil ditambahkan!")
+    else:
+        print(Fore.RED + f"\nID {id_baru} sudah ada.")
     input('< kembali(enter) ')
 
+# status done
 def read():
     tool.clear()
-    print(f"{'='*51}{'LIHAT DAFTAR GUDANG'}{'='*51}")
+    print(Fore.YELLOW + f"{" LIHAT DAFTAR PRODUK ":=^121}")
     dmn.menu_produk()
     input('< kembali(enter) ')
 
+# status done
 def update():
     tool.clear()
-    print(f"{'='*18}{'UBAH DATA GUDANG'}{'='*18}")
+    print(Fore.YELLOW + f"{" UBAH DATA PRODUK ":=^121}")
     dmn.menu_produk()
     
-    id_ubah= ehr.harus_nomor('Masukkan ID produk yang ingin diubah (atau ketik "x" untuk kembali): ')
+    konfirmasi = ehr.input_y_or_n('Apakah anda ingin mengubah Produk? (y/n): ')
+    if konfirmasi == 'y':
+        id = ehr.harus_nomor('Masukkan ID produk yang ingin diubah    : ')
+        if id in dpm.data_produk:
+            print(Fore.YELLOW + f"{f" Mengubah Data ID: {id} ":-^121}")
+            produk_lama = dpm.data_produk[id]
+            nama = ehr.tidak_kosong_capitalize(f"'Nama ({produk_lama['Nama Produk']})': ")
+            stok = ehr.harus_nomor(f"Stok ({produk_lama['Stok']}): ")
+            ukuran = ehr.harus_nomor(f"Ukuran/per Stok ({produk_lama['Ukuran/per Stok']}: ")
+            harga = ehr.harus_nomor(f"Harga (Rp{produk_lama['Harga']}: ")
+            tgl_masuk = ehr.input_tanggal(f"Tgl Masuk ({produk_lama['Tanggal Masuk']}): ")
 
-    if id_ubah not in dpm.data_produk:
-        print(f'ID Produk {id_ubah} tidak ditemukan.')
-    else:
-        print(f"--- Mengubah Data ID: {id_ubah} ---")
-        print("(Kosongi jika tidak ingin diubah)")
-        produk_lama = dpm.data_produk[id_ubah]
+            while True:
+                kadaluarsa = ehr.input_tanggal(f"Kadaluarsa ({produk_lama['Kadaluarsa']}): ")
+                if kadaluarsa > tgl_masuk:
+                    break
+                else:
+                    print(Fore.RED + "Tanggal kadaluarsa harus lebih besar dari tanggal masuk.")
+            while True:
+                print(f"Kategori ({produk_lama['Kategori']}): ")
+                print("\nkatagori: 1.Ruminansia 2.Unggas 3.Perikanan 4.Hewan Peliharaan")
+                pilihan = ehr.input_menu("pilih kategori (1/2/3/4): ")
+                if pilihan in [1, 2, 3, 4]:
+                    kategori = 'Ruminansia' if pilihan == 1 else 'Unggas' if pilihan == 2 else 'Perikanan' if pilihan == 3 else 'Hewan Peliharaan'
+                    break
+                else:
+                    print(Fore.RED + 'Pilihan tidak valid!.')
 
-        nama = ehr.tidak_kosong_capitalize(f"Nama ({produk_lama['Nama Produk']}): ")
-        stok = ehr.harus_nomor(f"Stok ({produk_lama['Stok']}): ")
-        ukuran = ehr.harus_nomor(f"Ukuran ({produk_lama['Ukuran/per Stok']}): ")
-        harga = ehr.harus_nomor(f"Harga (Rp{produk_lama['Harga']:,}): ")
-        tgl_masuk = ehr.input_tanggal(f"Tgl masuk ({produk_lama['Tanggal Masuk']}): ")
-        while True:
-            kadaluarsa = ehr.input_tanggal(f"Kadaluarsa ({produk_lama['Kadaluarsa']}): ")
-            if kadaluarsa > tgl_masuk:
-                break
-            else:
-                print("Tanggal kadaluarsa harus lebih besar dari tanggal masuk.")
-        while True:
-            print(f"Kategori ({produk_lama['Kategori']}): ")
-            print("\nkatagori: 1.Ruminansia 2.Unggas 3.Perikanan 4.Hewan Peliharaan")
-            pilihan = ehr.input_menu("pilih kategori (1/2/3/4)  :")
-            if pilihan in [1, 2, 3, 4]:
-                kategori = 'Ruminansia' if pilihan == 1 else 'Unggas' if pilihan == 2 else 'Perikanan' if pilihan == 3 else 'Hewan Peliharaan'
-                break
-            else:
-                print('Pilihan tidak valid!.')
+            dpm.data_produk[id] = {
+                "Nama Produk": nama,
+                "Stok": stok,
+                "Ukuran/per Stok": str(ukuran) + "Kg",
+                "Harga": harga,
+                "Kadaluarsa": kadaluarsa,
+                "Tanggal Masuk": tgl_masuk,
+                "Kategori": kategori
+            }
+            print(Fore.CYAN + 'Data pakan berhasil diubah!')
 
-        # Update
-        dpm.data_produk[id_ubah] = {
-            "Nama Produk": nama,
-            "Stok": stok,
-            "Ukuran/per Stok": str(ukuran) + "Kg",
-            "Harga": harga,
-            "Kadaluarsa": kadaluarsa if kadaluarsa else produk_lama['kadaluarsa'],
-            "Tanggal Masuk": tgl_masuk if tgl_masuk else produk_lama['tanggal masuk'],
-            "Kategori": kategori if kategori else produk_lama['kategori']
-        }
-        print('Data pakan berhasil diubah!')
-
+        else:
+            print(Fore.RED + f'ID Produk {id} tidak ditemukan.')
+    elif konfirmasi == 'n':
+        print('keluar menu ubah produk')
     input('< kembali(Enter) ')
 
+# status done
 def delate():
+    tool.clear()
+    print(Fore.YELLOW + f'{" HAPUS DATA PRODUK ":=^121}')
     dmn.menu_produk()
-    
-    id_produk = input("Masukkan ID produk yang mau dihapus: ")
 
-    if id_produk not in dpm.data_produk:
-        print("ID tidak ada.")
-        return
-    
-    konfirmasi = input("Hapus produk? (y/n): ").lower()
-    if konfirmasi == 'y':
-        del dpm.data_produk[id_produk]
-        print("Produk berhasil dihapus!")
-    else:
-        print("Penghapusan dibatalkan.")
+    pilihan = ehr.input_y_or_n('Apakah mau hapus produk? (y/n): ')
+    if pilihan == 'y':
+        id_produk = ehr.harus_nomor("Masukkan ID produk yang mau dihapus: ")
+        if id_produk in dpm.data_produk:
+            konfirmasi = ehr.input_y_or_n('Hapus produk? (y/n): ')
+            if konfirmasi == 'y':
+                del dpm.data_produk[id_produk]
+                print(Fore.CYAN + "Produk berhasil dihapus!")
+            else:
+                print("Penghapusan dibatalkan.")
+        else:
+            print(Fore.RED + "ID produk tidak ditemukan!")
+    elif pilihan == 'n':
+        print('Keluar dari menu delete.')
+    input('< kembali (enter) >')
 
+# status done
 def history():
     tool.clear()
     try:
--- display_menu.py
+++ display_menu.py
@@ -94,28 +94,28 @@ def menu_visualisasi():
 
 def menu_produk():
     table_data = []
-    for ID, info in dpm.data_produk.items():
+    for key_id, velues in dpm.data_produk.items():
         table_data.append([
-            ID,
-            info["Nama Produk"],
-            info["Stok"],
-            info["Ukuran/per Stok"],
-            f"Rp{info['Harga']:,}",
-            info["Kadaluarsa"],
-            info["Tanggal Masuk"],
-            info["Kategori"]
+            key_id,
+            velues["Nama Produk"],
+            velues["Stok"],
+            velues["Ukuran/per Stok"],
+            f"Rp{velues['Harga']:,}",
+            velues["Kadaluarsa"],
+            velues["Tanggal Masuk"],
+            velues["Kategori"]
         ])
     print(tb.tabulate(table_data, headers=["ID", "Nama Produk", "Stok", "Ukuran/Stok", "Harga", "Kadaluarsa", "Tanggal Masuk", "Kategori"], tablefmt="rounded_grid", colalign=("center", "left", "center", "center", "right", "center", "center", "left")))
 
 
 def tabel_keranjang(keranjang_user):
     data_tabel = []
-    for id_produk, item in keranjang_user.items():
+    for key_id, velues in keranjang_user.items():
         baris = [
-            id_produk,
-            item['nama_produk'],
-            item['jumlah'],
-            f"Rp {item['harga_satuan']:,}",
+            key_id,
+            velues['nama_produk'],
+            velues['jumlah'],
+            f"Rp {velues['harga_satuan']:,}",
         ]
         data_tabel.append(baris)
     print(tb.tabulate(data_tabel, headers=["ID produk", "Nama Produk", "Jumlah", "Harga Satuan"], tablefmt="rounded_grid", colalign=("center", "left", "center", "right")))
